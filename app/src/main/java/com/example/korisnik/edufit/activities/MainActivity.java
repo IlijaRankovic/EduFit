7eece6cc98301713046b2a24f86483610974741e
Ilija Rankovic <ilijarankovic@gmail.com>
2017-05-24 11:37:36 +0200

    Nije sve zavrseno fixes #2

---
+++ app/src/main/java/com/example/korisnik/edufit/activities/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.korisnik.edufit.activities;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Korisnik on 19.5.2017..
+ */
+
+public class MainActivity extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+
+        setContentView();
+    }
+}
